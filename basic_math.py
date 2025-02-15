ec5a69a5627d501df4b9f94e4e869a2426d0d44a
Joanna Keza <joanna.keza@stu.ocsb.ca>
2025-02-15 15:02:15 +0000

    Added

---
+++ basic_math.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# Created By: Joanna Keza
+# Date: February 14, 2025
+# This program does basic maths.
+
+
+def main():
+    print("9+3={}".format(9 + 3))
+    print("6-10={}".format(6 - 10))
+    print("5*5={}".format(5 * 5))
+    print("10/3={}".format(10 / 3))
+    print("7+10*7={}".format(7+10*7))
+    print("8**4={}".format(8**4))  # exponent
+    print("100**0.5={}".format(100**0.5))  # square root
+
+
+if __name__ == "__main__":
+    main()
